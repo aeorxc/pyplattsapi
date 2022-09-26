a5c0d72f5198ba3d02f494771ffe927dd62396fa
UI935452 <josh.alexander@rwe.com>
2022-09-26 15:34:32 +0100

    Added margins data

--- runs.py
+++ runs.py
@@ -47,5 +47,37 @@ def getCountryRunsTimeSeries(country: str):
 
     return df7
 
+def getMarginsbyType(type : str):
+    Headers = {'accept': 'application/json', 'appkey': os.getenv('API_KEY')}
+    Historical_data_URL = f"https://api.platts.com/odata/refinery-data/v2/Margins?&pageSize=1000&$expand=*"
+    df5 = pd.DataFrame()
+
+    while Historical_data_URL != 'NaN':
+
+        time.sleep(1)  # api can only accept 2 requests per second and 5000 per day
+        data_request = requests.get(url=f'{Historical_data_URL}', headers=Headers)
+        data = data_request.json()
+        df2 = pd.json_normalize(data).reset_index(drop=True)
+
+        x = df2['value'].iloc[0]
+        df3 = pd.json_normalize(x).reset_index(drop=True)
+        df3 = df3.drop_duplicates()
+        df5 = df5.append(df3, ignore_index=False)
+
+        try:
+            Historical_data_URL = df2[f'@odata.nextLink'].iloc[0]
+
+        except:
+
+            Historical_data_URL = 'NaN'
+
+            continue
+    df5 = df5[df5['MarginType.Name'] == type]
+    df5 = df5.reset_index()
+    return df5
+
+
+
 if __name__ == '__main__':
-    print(getCountryRunsTimeSeries('China'))
+    #print(getCountryRunsTimeSeries('China'))
+    print(getMarginsbyType('Dated Brent NWE Cracking'))
