3aff56d5dcbb136d72a10be9f2c9491737f88011
ui906879 <gibran.afzal@rwe.com>
2022-10-14 14:58:34 +0100

    feat: add get_metadata()

--- pyplattsapi/world_oil_supply.py
+++ pyplattsapi/world_oil_supply.py
@@ -6,6 +6,7 @@ from pyplattsapi import plattsapicore
 
 api_name = "WORLD OIL SUPPLY"
 production_api = f"{plattsapicore.api_url}/wos/v2/production"
+metadata_api = f"{plattsapicore.api_url}/wos/v2/metadata_endpoint"
 
 
 def get_production(filter, field, groupBy, scenarioTermId: int = 2):
@@ -19,6 +20,21 @@ def get_production(filter, field, groupBy, scenarioTermId: int = 2):
     }
     res = plattsapicore.generic_api_call(api=production_api, api_name=api_name, params=params)
 
-    res.index = res.apply(lambda x: pd.to_datetime(f"{x.year}-{x.month}-1"), 1)
-    res.index.name = "date"
+    if 'month' in res.columns and 'year' in res.columns:
+        res.index = res.apply(lambda x: pd.to_datetime(f"{x.year}-{x.month}-1"), 1)
+        res.index.name = "date"
     return res
+
+
+def get_metadata(metadata_endpoint:str, filter:str=None, field:str=None, groupBy:str=None) -> pd.DataFrame:
+    params = {
+        "filter": filter,
+        "field": field,
+        "pageSize": 1000,
+        "groupBy": groupBy,
+        "page": 1,
+    }
+    url = metadata_api.replace('metadata_endpoint', metadata_endpoint)
+    res = plattsapicore.generic_api_call(api=url, api_name=api_name, params=params)
+    return res
+
--- tests/test_world_oil_supply.py
+++ tests/test_world_oil_supply.py
@@ -22,3 +22,26 @@ from pyplattsapi import world_oil_supply
 def test_get_production(filter: str, field: str, groupBy: str):
     res = world_oil_supply.get_production(filter=filter, field=field, groupBy=groupBy)
     assert isinstance(res, pd.DataFrame)
+
+
+@pytest.mark.parametrize(
+    "metadata_endpoint, filter, field, groupBy",
+    [
+        (
+            "grade-elements",
+            'gradeElementName:"Zarzaitine"',
+            None,
+            None,
+        ),
+        (
+            "countries",
+            None,
+            None,
+            None,
+        ),
+
+    ],
+)
+def test_get_metadata(metadata_endpoint:str, filter: str, field: str, groupBy: str):
+    res = world_oil_supply.get_metadata(metadata_endpoint=metadata_endpoint, filter=filter, field=field, groupBy=groupBy)
+    assert isinstance(res, pd.DataFrame)
