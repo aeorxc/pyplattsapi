2b6f8fcc4fb030bdb65ccc57b3045df1a88ad710
ui906879 <gibran.afzal@rwe.com>
2022-10-18 14:42:26 +0100

    fix: env variables in .github

--- rebuild.bat
+++ rebuild.bat
@@ -1,3 +1,3 @@
 pip uninstall -y pyplattsapi
 python setup.py bdist_wheel
-pip install C:\dev\pyplattsapi\dist\pyplattsapi-0.0.2-py3-none-any.whl
+pip install C:\dev\pyplattsapi\dist\pyplattsapi-0.0.3-py3-none-any.whl
