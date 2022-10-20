f5e524f3e7e1526383066b9b03f0a3cebecf66e0
UI935452 <josh.alexander@rwe.com>
2022-10-20 15:16:03 +0100

    Added in get price forecast function

--- pyplattsapi/price_forecast.py
+++ pyplattsapi/price_forecast.py
@@ -7,12 +7,20 @@ api_name = "WORLD REFINERY DATABASE"
 
 def get_price_forecast(symbol: str):
     forecasts_api = f"{plattsapicore.api_url}/energy-price-forecast/v1/prices-short-term"
-    filter = "priceSymbol: " + f'"{symbol}"'
+    filter = "priceSymbol: " + f'"{symbol}"' + "AND categoryName: " + '"Oil"'
     params = {
         "filter": filter,
         "pageSize": 1000,
     }
-    res = plattsapicore.generic_api_call(api=forecasts_api, api_name=api_name, params=params)
+    try:
+        res = plattsapicore.generic_api_call(api=forecasts_api, api_name=api_name, params=params)
+    except:
+        filter = "priceSymbol: " + f'"{symbol}"' + "AND categoryName: " + '"NGL"'
+        params = {
+            "filter": filter,
+            "pageSize": 1000,
+        }
+        res = plattsapicore.generic_api_call(api=forecasts_api, api_name=api_name, params=params)
     res.index = res.apply(lambda x: pd.to_datetime(f"{x.year}-{x.month}-1"), 1)
     res.index.name = "date"
     return res
